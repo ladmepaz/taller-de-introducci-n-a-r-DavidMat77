3b41aafaf38dbc937bb6d524794922d06c941b13
DavidMat77 <jmattos@unal.edu.co>
2024-10-31 15:26:45 -0500

    Update ejercicio06.R

--- ejercicio06.R
+++ ejercicio06.R
@@ -2,14 +2,11 @@
 
 solucion <- function() {
   # Ordena las asignaciones de manera que se devuelva un valor de 's' igual a 43
-  s <- NULL
-  a <- NULL
-  b <- NULL
-  s <- a+b
-  a <- 8
-  s <- s+a
-  b <- 20
-  s <- s+5
-  a <- 10
+  a <- 8       
+  b <- 20      
+  s <- a + b   
+  s <- s + 5   
+  a <- 10      
+  s <- s + a  
   return(s)
 }
